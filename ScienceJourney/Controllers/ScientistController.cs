6c84c108741942a5e0c8f95bf956d5c8bfda9278
Erhan Gidici <erhan_gidici@hotmail.com>
2016-07-11 22:44:20 +0300

    Angular Select

--- ScienceJourney/Controllers/ScientistController.cs
+++ ScienceJourney/Controllers/ScientistController.cs
@@ -1,4 +1,6 @@
-﻿using ScienceJourney.Models;
+﻿using log4net;
+using ScienceJourney.DAL;
+using ScienceJourney.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,23 +12,53 @@ namespace ScienceJourney.Controllers
 {
     public class ScientistController : Controller
     {
+        private ScienceJourneyEntities2 db = new ScienceJourneyEntities2();
+        static ILog log = log4net.LogManager.GetLogger(typeof(HomeController));
+
         //
         // GET: /Scientist/
         public ActionResult Index()
         {
-            string[] filePaths = Directory.GetFiles(Server.MapPath("~/assets/img/"));
-            List<Slider> files = new List<Slider>();
-            foreach (string filePath in filePaths)
-            {
-                string fileName = Path.GetFileName(filePath);
-                files.Add(new Slider
-                {
-                    title = fileName.Split('.')[0].ToString(),
-                    src = "../assets/img/" + fileName
-                });
-            }
+            //string[] filePaths = Directory.GetFiles(Server.MapPath("~/assets/img/"));
+            //List<Slider> files = new List<Slider>();
+            //foreach (string filePath in filePaths)
+            //{
+            //    string fileName = Path.GetFileName(filePath);
+            //    files.Add(new Slider
+            //    {
+            //        title = fileName.Split('.')[0].ToString(),
+            //        src = "../assets/img/" + fileName
+            //    });
+            //}
 
-            return View(files);
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetScientistById(int id)
+        {
+            try
+            {
+                Scientist _scien = new Scientist();
+                _scien = db.Scientists.Find(id);
+                return Json(_scien, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                log.Info(String.Format("Exception occurred"));
+                log.Error(ex.Message);
+            }
+            return Json(null);
+        
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
 	}
 }
---
+++ ScienceJourney/Scripts/DB/GetScientist.js
@@ -0,0 +1,15 @@
+﻿var myApp = angular.module('myApp', []);
+
+myApp.controller('scientistCtrl', function ($scope, $http) {
+
+    $scope.scientist = "";
+    $scope.selectScientist = function (id) {
+        $http.post("/Scientist/GetScientistById", { id: id })
+        .success(function(result) {
+            $scope.scientist = result;
+        })
+        .error(function(result) {
+            console.log(result);
+        })
+    }
+});
